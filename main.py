a772b7181ef57bf016e66438e8c17cda3724df35
Arminaz <arminaz321@hotmail.ca>
2025-02-19 06:06:58 -0700

    Finished the Project to Get Name dictionary based on Nato Phonetic Alphabet

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
+import pandas
+
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+all_alphabet_dict = []
+for current_student in student_dict['student']:
+    alphabet_dict = {letter:data.code[data.letter == letter.upper()].iloc[0] for letter in current_student}
+    all_alphabet_dict.append(alphabet_dict)
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# List of all Names with phonetic alphabet.
+print(all_alphabet_dict)
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+user_input = input("Enter a Name to get the Phonetic Alphabet: ")
+alphabet_dict = {}
+while user_input != "Exit":
+    alphabet_dict = {letter:data.code[data.letter == letter.upper()].iloc[0] for letter in user_input}
+    print(alphabet_dict)
+    user_input = input("Enter a Name to get the Phonetic Alphabet: ")
